7cfad66fddf97c45d499a45eba31a83ea132b6d8
septillioner <egeismailkosedag@gmail.com>
2026-07-31 13:04:00 +0300

    chore: release v2.3.0

--- extensions/vs2026/Pablo/Properties/AssemblyInfo.cs
+++ extensions/vs2026/Pablo/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Pablo")]
 [assembly: AssemblyCopyright("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.2.3.0")]
-[assembly: AssemblyFileVersion("2.2.3.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
